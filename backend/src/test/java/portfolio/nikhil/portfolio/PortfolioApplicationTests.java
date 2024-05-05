06a2d4b94340e39535eb4bb066440a5eb075e217
NikhilRajivOke <nikhiloke97@gmail.com>
2024-05-05 14:18:46 -0400

    Initial portfolio setup

---
+++ backend/src/test/java/portfolio/nikhil/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package portfolio.nikhil.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
